32a51741cfa6ccb6c85754c871b411097d011662
Narendra Gurav <narendragurav@Narendras-MacBook-Air.local>
2025-10-25 21:13:27 +0530

    Replace bcrypt with bcryptjs for Render compatibility

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { encrypt, decrypt } = require('../utils/encryption');
 
 const userSchema = new mongoose.Schema({
